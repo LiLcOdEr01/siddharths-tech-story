fb1522664607e2012183d4720852be1d5016a578
Siddharth Singh <116557034+LiLcOdEr01@users.noreply.github.com>
2025-09-27 21:20:54 +0000

    added resume link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,10 +32,12 @@ const Hero = () => {
             <Mail className="mr-2 h-5 w-5" />
             Get In Touch
           </Button>
-          <Button variant="outline" size="lg" className="border-border hover:bg-secondary font-semibold px-8 py-3">
+          
+          <a href="https://drive.google.com/file/d/1HWFDjCmGJU1r_6NLYxvwk-OH-Aubm_oa/view?usp=sharing"><Button variant="outline" size="lg" className="border-border hover:bg-secondary font-semibold px-8 py-3">
             <Download className="mr-2 h-5 w-5" />
             Download Resume
           </Button>
+          </a>
         </div>
 
         {/* Social Links */}
